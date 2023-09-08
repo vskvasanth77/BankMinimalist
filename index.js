3a52e9bc0f57201c56e3ea406ed5699be70f5f9a
Vasanth <vskvasanth777@gmail.com>
2023-09-08 12:05:45 +0530

    sticky nav added using observer api intersection

--- index.js
+++ index.js
@@ -111,12 +111,29 @@ nav.addEventListener("mouseover", handlerHover.bind(0.5));
 nav.addEventListener("mouseout", handlerHover.bind(1));
 
 //stick navbar
-const obsCallback = function (entries, observer) {
-  entries.forEach((entry) => console.log(entry));
+// const obsCallback = function (entries, observer) {
+//   entries.forEach((entry) => console.log(entry));
+// };
+// const obsOptions = {
+//   root: null,
+//   threshold: [0,0.2],
+// };
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
 };
-const obsOptions = {
+const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
-  threshold: 0.1,
-};
-const observer = new IntersectionObserver(obsCallback, obsOptions);
-observer.observe(section1);
+  threshold: 0,
+  rootMargin: `${navHeight}px`,
+});
+headerObserver.observe(header);
