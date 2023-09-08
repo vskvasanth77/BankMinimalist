7e5efcaa64bb5d6e90a3007919ca3b5224156ca9
Vasanth <vskvasanth777@gmail.com>
2023-09-08 12:27:38 +0530

    section observer api added

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
 
   if (!entry.isIntersecting) nav.classList.add("sticky");
   else nav.classList.remove("sticky");
@@ -137,3 +137,22 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `${navHeight}px`,
 });
 headerObserver.observe(header);
+
+//reveal animation section
+
+const allSections = document.querySelectorAll(".section");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
